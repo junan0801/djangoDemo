fe16aec58c686bfb05e4e0ff7010618c533a9e2f
jun-home <jun@gmail.com>
2022-05-12 12:16:13 +0800

    add mysql conncetion

---
+++ mysql/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20, verbose_name='名称')
+    bpub_data = models.CharField(max_length=20, verbose_name='发布日期')
+
+    class Meta:
+        db_table = 'tb_books'
+        verbose_name = '图书'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.btitle
+
